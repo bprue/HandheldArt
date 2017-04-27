16e28d4f40928365a3ca5aa1c58a9f83996c82e7
bprue <pruette95@gmail.com>
2017-04-27 18:54:40 -0400

    Added in nil checks, updated save-to-device longpress

--- PopUpViewController.swift
+++ PopUpViewController.swift
@@ -11,9 +11,20 @@ import UIKit
 class PopUpViewController: UIViewController, UIScrollViewDelegate {
     
     var myImageURL:URL!
+    
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
+
+
+    @IBAction func backButton(_ sender: Any) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,18 +33,39 @@ class PopUpViewController: UIViewController, UIScrollViewDelegate {
         self.scrollView.minimumZoomScale = 1.0
         
         self.scrollView.maximumZoomScale = 6.0
+        
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPressed))
+
+        
+        
+        imageView.addGestureRecognizer(longPressRecognizer)
     }
 
+    
+    func longPressed(sender: UILongPressGestureRecognizer) {
+        //have pop up that says copy or save photo
+        
+        
+        let alertController = UIAlertController(title: "Handheld Art", message:
+            "Save image to device?", preferredStyle: UIAlertControllerStyle.alert)
+        let alertActionOkay = UIAlertAction(title: "Save", style: .default) { (_) in UIImageWriteToSavedPhotosAlbum(self.imageView.image!, nil, nil, nil)
+        }
+        let alertActionCancel = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        alertController.addAction(alertActionOkay)
+        alertController.addAction(alertActionCancel)
+        
+
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    @IBAction func closePopUp(_ sender: AnyObject) {
-        
-        dismiss(animated: true, completion: nil)
-    }
+
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return self.imageView
